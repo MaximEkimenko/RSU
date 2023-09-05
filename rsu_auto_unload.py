3a5caab5f0a10b581e287cf09c48b7be032ae48a
MaximEkimenko <ekimenko.m@gmail.com>
2023-09-05 18:57:55 +0600

    timeouts corrections

--- rsu_auto_unload.py
+++ rsu_auto_unload.py
@@ -2,22 +2,34 @@ import datetime
 import logging
 import os
 import time
+from selenium.webdriver.support import expected_conditions
 import openpyxl
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.by import By
-from madmodule import create_list
-import shutil
+import warnings
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.wait import WebDriverWait
+
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "none"
+
+warnings.filterwarnings('ignore')
+# from madmodule import create_list
+# import shutil
 
 logging.basicConfig(level=logging.ERROR, filename='log.txt', filemode="a+",
                     format="%(asctime)-15s %(levelname)-8s %(message)s")
 logger = logging.getLogger('logger')
 
+
 # logger.setLevel(logging.CRITICAL)
 # logging.disable(logging.CRITICAL)
 
+
 def auto_unload():
-    # TODO добавить логи вместо принтов
+    # TODO добавить логи вместо prints
     # инициализация словаря предыдущих значений
     rsu_previews_data = {'1-1': set(), '2-1': set(), '3-1': set(), '4-1': set(), '4-2': set(), '4-3': set(),
                          '4-4': set(), '5-1': set(), '6-1': set()}
@@ -44,15 +56,20 @@ def auto_unload():
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         try:
             driver = webdriver.Chrome(options=options)  # назначение драйвера
-            driver.set_page_load_timeout(40)
         except Exception as e:
             print(e, 'Ошибка создания webdriver. Отсутствует связь.')
+        try:
+            # driver.set_page_load_timeout(40)
+            # driver.set_script_timeout(40)
+            driver.implicitly_wait(10)
+            # driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        except Exception as e:
+            print(e, 'Ошибка timeout!')
         for rsu_name in rsu.keys():
             # result_file_name = rf"{os.getcwd()}\Выгрузка РСУ {rsu_name} за {today}.xlsx"  # имя xlsx файла
             result_file_name = rf"O:\Расчет эффективности\Выгрузки РСУ\Выгрузка РСУ {rsu_name} за {today}.xlsx"
             rsu_result_list = []  # список результатов
             time_sum = 0  # сумма дуги
-            sum_str = ''  # строка слагаемых суммы дуги
             try:
                 print('Запуск для:', rsu[rsu_name]['ip'], rsu_name, time_now_str, today)
                 try:
@@ -92,12 +109,16 @@ def auto_unload():
                                                   'стоимость минуты дуги'])
                             arc_sum_before = 0
                         driver.get(rsu[rsu_name]['ip'])
-                        time.sleep(30)
+                        # time.sleep(30)
+                        # ожидание появления элемента
+                        element = WebDriverWait(driver, 30).until(
+                            expected_conditions.presence_of_element_located((By.ID, "tablecontainer")))
+                        driver.implicitly_wait(20)  # неявное ожидание 20 сек
                         data = driver.find_elements(By.CLASS_NAME, 'rowElement')
                         for row in data:  # обработка строк для отчёта
                             line = row.text  # текст элемента
                             if line not in rsu_previews_data[rsu_name]:
-                            #  print(line)
+                                # print(line)
                                 coma_index = line.find(',')  # индекс запятой
                                 if coma_index != -1:
                                     arc_date = line[coma_index - 10:coma_index]
@@ -118,11 +139,11 @@ def auto_unload():
                                 if str(arc_date).strip() == str(today).strip():
                                     rsu_result_list.append([rsu_name, arc_date, arc_period, float(arc_time), '',
                                                             line])
-                                                            # line.replace('\n', '')])
+                                    # line.replace('\n', '')])
                                     time_sum = time_sum + float(arc_time)
                                     res_sh.append([rsu_name, arc_date, arc_period, float(arc_time), '',
                                                    line])  # строка в исходном виде для сравнения
-                                                   # line.replace('\n', '')]) #
+                                    # line.replace('\n', '')]) #
                                     rsu_previews_data[rsu_name].add(line)  # добавление строки в уже выгруженные
                                     print(f'Заполнено.')
                                     # print(f'Заполнено: {line}')
@@ -140,7 +161,7 @@ def auto_unload():
                         sum_result_sh['A2'] = today
                         sum_result_sh['D2'] = rsu_name
                         sum_result_sh['G2'] = rsu[rsu_name]['location']
-                        print(rsu[rsu_name]['location'])
+                        # print(rsu[rsu_name]['location'])
                         sum_result_sh['H2'] = round(arc_value, 2)
                         sum_result_sh['I2'] = 4
                         sum_result_sh['J2'] = str(sum_result_sh['J2'].value) + '+' + f"{round(time_sum / 60 / 60, 2)}"
@@ -150,27 +171,30 @@ def auto_unload():
                             print(f'Файл {result_file_name} обновлен.')
                         else:
                             print(f'Данные не изменились. Файл {result_file_name} НЕ обновлен.')
+                        ActionBuilder(driver).clear_actions()
+                        # driver.close()
                     else:
                         print(rsu_name, 'Недоступен.')
+
             except Exception as e:
                 print(e)
         reset_time_interval_start = datetime.datetime.strptime("00:01", "%H:%M")
-        reset_time_interval_end = datetime.datetime.strptime("00:41", "%H:%M")
+        reset_time_interval_end = datetime.datetime.strptime("00:21", "%H:%M")
         yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%d.%m.%Y')  # вчера
-        # сохранение результатов в сетевую папку при попадании в reset_time интервал
+        # сброс previews data
         if reset_time_interval_end > time_now > reset_time_interval_start:
             try:
-                file_list = create_list(dir_path=os.getcwd(), result_type='files', extension=f'{yesterday}.xlsx')
-                print(file_list)
-                for file_name in file_list:
-                    shutil.copy(file_name, r"O:\Расчет эффективности\Выгрузки РСУ")
-                    print(f'{file_name} copied!')
+                # file_list = create_list(dir_path=os.getcwd(), result_type='files', extension=f'{yesterday}.xlsx')
+                # print(file_list)
+                # for file_name in file_list:
+                #     shutil.copy(file_name, r"O:\Расчет эффективности\Выгрузки РСУ")
+                #     print(f'{file_name} copied!')
                 rsu_previews_data = {'1-1': set(), '2-1': set(), '3-1': set(), '4-1': set(), '4-2': set(),
                                      '4-3': set(),
                                      '4-4': set(), '5-1': set(), '6-1': set()}
                 print(f'rsu_previews_data reset complete!')
             except Exception as e:
-                print(e, "ФАЙЛЫ НЕ СКОПИРОВАНЫ!")
+                print(e, "rsu_previews_data reset NOT complete!")
         print('Итерация пройдена, ожидание следующей. Запуск через 2 минуты.')
 
         time.sleep(2 * 60)
